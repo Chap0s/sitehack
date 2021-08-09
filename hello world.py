2a4529a5d390aff2aa273f3a888cf3f2971ddddc
Chap0s <88678407+Chap0s@users.noreply.github.com>
2021-08-09 20:07:35 +0300

    sitehack.py

---
+++ hello world.py
@@ -0,0 +1,25 @@
+import time
+print("""
+=========================     
+====Chap0s=HACK-ARACI====
+========================= 
+      """)
+nick = input("Nick: ")
+hedefsite = input("Hedef Site(orneksite.com): ")
+
+print(" Hedef site hackleniyor")
+time.sleep(1)
+print("%25")
+time.sleep(1)
+print("%50")
+time.sleep(1)
+print("%75")
+time.sleep(1)
+print("%85")
+time.sleep(1)
+print("%95")
+time.sleep(1)
+print("%100")
+print("Bir saniye veri tabanı çekiliyor.")
+time.sleep(3)
+print("Şuan hackledin işte son model lamer seni :D")
